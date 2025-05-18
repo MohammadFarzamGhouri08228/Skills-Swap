6cb08cc542ff1e64903809ee34d1fce474e9545a
MohammadFarzamGhouri08228 <mg08228@st.habib.edu.pk>
2025-05-18 13:51:36 +0500

    deploy fix

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+// src/app/signup/page.tsx
+export default function SignupPage() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        {/* Add form or content */}
+      </div>
+    );
+  }
+  
