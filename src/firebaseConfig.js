3d625fb781e372e8fd517e23b6855df59cf949b9
MohammadFarzamGhouri08228 <mg08228@st.habib.edu.pk>
2025-05-19 16:41:22 +0500

    fix firebase.config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,8 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDK547wdEyC5rDDSO87MPORrEHWNC8ECSw",
+   apiKey: "AIzaSyDK547wdEyC5rDDSO87MPORrEHWNC8ECSw",
   authDomain: "skillswap-cf57c.firebaseapp.com",
+  projectId: "skillswap-cf57c",
+  storageBucket: "skillswap-cf57c.firebasestorage.app",
+  messagingSenderId: "747945090512",
+  appId: "1:747945090512:web:acb0385ed6bfe0b09fedd5",
+  measurementId: "G-F4GZ68HD6G"
   // ...other config values from your Firebase project
 };
 
